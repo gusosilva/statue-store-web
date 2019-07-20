1d71f2172a9dc7bcb532a39cb063245302f076b0
Gustavinho <gustavolive22@gmail.com>
2019-07-20 17:49:54 -0300

    ready for Azure usage

--- StatueStoreWebApplic/Models/BDConexao.cs
+++ StatueStoreWebApplic/Models/BDConexao.cs
@@ -12,7 +12,7 @@ namespace StatueStoreWebApplic.Models {
             command.Connection = connection;
         }
 
-        public SqlConnection connection = new SqlConnection("Server=localhost;Database=StatueStore;Trusted_Connection=True;");
+        public SqlConnection connection = new SqlConnection("Server=statuestore-server.database.windows.net;Database=statuestore;User ID=gustavo123;Password=Dd2460500;Trusted_Connection=False;");
         public SqlCommand command = new SqlCommand();
 
     }
---
+++ StatueStoreWebApplic/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StatueStoreWebApplic.MvcApplication" Language="C#" %>
